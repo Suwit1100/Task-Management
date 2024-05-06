627fa37f9b700d8888ba01f8c343c5ad63c20a48
สุวิทย์ ลีเจ้ยวาระ <139098129+Suwit1100@users.noreply.github.com>
2024-05-06 16:36:26 +0700

    style

--- task-management/src/components/Item.jsx
+++ task-management/src/components/Item.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import '../css/Item.css'
 function Item() {
     return (
-        <>
+        <div className='list-item'>
             <p>รายการที่ 1</p>
-        </>
+            <div className="btn-group">
+                <button className='btn-edit'>แก้ไข</button>
+                <button className='btn-delete'>ลบ</button>
+            </div>
+        </div>
     )
 }
 
